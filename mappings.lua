1bb478a426009929398a22337b08726384cf4f9c
jered.little <jeredlittle1996@gmail.com>
2023-04-16 10:36:18 -0600

    Changed bindings for WSL and wrap

--- mappings.lua
+++ mappings.lua
@@ -23,11 +23,11 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    ["<leader>a"] = { "", desc = "Windows WSL Commands" },
+    ["<leader>W"] = { "<cmd>echo 'Select a Command'<cr>", desc = "Windows WSL Commands" },
     ["<leader>w"] = { ":set wrap linebreak nolist<cr>", desc = "Wrap Text" },
-    ["<leader>W"] = { ":set nowrap<cr>", desc = "No Wrap Text" },
-    ["<leader>aC"] = { ":'<,'>w !clip.exe", desc = "Copy Selection to Clipboard" },
-    ["<leader>aCf"] = { ":w !clip.exe", desc = "Copy File to Clipboard" },
+    ["<leader>wx"] = { ":set nowrap<cr>", desc = "No Wrap Text" },
+    ["<leader>Wc"] = { ":'<,'>w !clip.exe<cr>", desc = "Copy Selection to Clipboard" },
+    ["<leader>Wf"] = { ":w !clip.exe<cr>", desc = "Copy File to Clipboard" },
   },
   t = {
     -- setting a mapping to false will disable it
