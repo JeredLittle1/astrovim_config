97a154a58c525644f6022c0e258e3e6c0391ac29
jered.little <jeredlittle1996@gmail.com>
2023-04-27 03:05:30 -0600

    removed vimwiki

--- plugins/vimwiki.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "vimwiki/vimwiki",
-    lazy = false,
-    init = function() --replace 'config' with 'init'
-      vim.g.vimwiki_list = { { path = '~/wiki', syntax = 'markdown', ext = '.md' } }
-    end
-  },
-}
