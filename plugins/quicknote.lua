96539cf9185a6f2a88e6f23c55e938bc230c9149
jered.little <jeredlittle1996@gmail.com>
2023-04-29 14:07:45 -0600

    Removed unneeded plugins

--- plugins/quicknote.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "RutaTang/quicknote.nvim",
-    config = {
-      mode = "portable" -- "portable" | "resident", default to "portable"
-    },
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
-}
