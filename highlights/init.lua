62db1075796785e428864d3299a8402e3068aac6
Jered <jeredlittle1996@gmail.com>
2023-06-11 06:22:13 -0600

    Added missing transparent modes

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,11 +1,12 @@
--- require("notify").setup({
---   background_colour = "#000000",
--- })
+require("notify").setup({
+  background_colour = "#000000",
+})
 return {
   -- this table overrides highlights in all themes
-  -- Normal = { bg = "NONE", ctermbg = "NONE" },
-  -- NormalNC = { bg = "NONE", ctermbg = "NONE" },
-  -- CursorColumn = { bg = "NONE", ctermbg = "NONE" },
+  -- Normal = { bg = "NONE", ctermbg = "NONE", ctermfg = "NONE" },
+  -- EndOfBuffer = { bg = "NONE", ctermbg = "NONE", ctermfg = "NONE" },
+  -- NormalNC = { bg = "NONE", ctermbg = "NONE", ctermfg = "NONE" },
+  -- CursorColumn = { bg = "NONE", ctermbg = "NONE", ctermfg = "NONE" },
   -- CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
   -- CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
   -- LineNr = {},
@@ -13,6 +14,7 @@ return {
   -- StatusLine = {},
   -- NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
   -- NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+  -- NeoTreeEndOfBuffer = { bg = "NONE", ctermbg = "NONE", ctermfg = "NONE" },
   -- WinBar = { bg = "NONE", ctermbg = "NONE" },
   -- WinBarNC = { bg = "NONE", ctermbg = "NONE" }
 }
